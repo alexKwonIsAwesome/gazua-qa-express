a753132d984777b28e6f8ffe683723d8ddb5bc8a
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2018-08-17 15:08:56 +0900

    add graphql server and its type def & resolvers
    Wrote unmodulated graphql server.ts file. 
    
    Currently the server implements ApolloServer. It now has creation, retrieval of questions and answers. And two models have 1:M relation, by foreign key 'questionId' of Answer.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,149 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import {
+  ApolloServer,
+  gql,
+  ApolloError
+} from 'apollo-server-express';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require('../service-account.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
 
 const app = express();
 
-// middlewares
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// GraphQL types
+const typeDefs = gql`
+  type Question {
+    id: ID!
+    question: String
+    contents: String
+    answers: [Answer!]!
+  }
 
-app.get('/', (req, res) => res.send('Hello World!'));
+  type Answer {
+    contents: String
+    questionId: String!
+  }
 
-app.listen(7777, () => console.log('Hi!!'));
+  type Query {
+    questions: [Question!]!
+    question(id: String!): Question
+    answers: [Answer!]!
+    answer(id: String!): Answer
+  }
+
+  type Mutation {
+    addQuestion(question: String, contents: String): Question
+    addAnswer(questionId: ID!, contents: String): Answer
+  }
+`;
+
+// interfaces
+
+interface IQuestion {
+  id: string
+  question: string
+  contents: string
+}
+
+interface IAnswer {
+  id: string
+  contents: string
+  questionId: string
+}
+
+const resolvers = {
+  Query: {
+    async questions() {
+      try {
+        const questionSnapshots = await db.collection('questions').get();
+        return questionSnapshots.docs.map((snapshot) => {
+          return snapshot.data();
+        });
+      } catch (e) {
+        throw new ApolloError(e);
+      }
+    },
+    async question(root: null, args: any) {
+      try {
+        const { id } = args;
+        const questionDoc = await db.collection('questions').doc(id).get();
+        return questionDoc.data();
+      } catch (e) {
+        throw new ApolloError(e);
+      }
+    },
+    async answers() {
+      try {
+        const answerSnapshots = await db.collection('answers').get();
+        return answerSnapshots.docs.map((snapshot) => {
+          return snapshot.data();
+        });
+      } catch (e) {
+        throw new ApolloError(e);
+      }
+    }
+  },
+  Mutation: {
+    async addQuestion(root: null, args: any) {
+      try {
+        const { question, contents } = args;
+        const newQuestionRef = db.collection('questions').doc();
+        await newQuestionRef.set({
+          id: newQuestionRef.id,
+          question,
+          contents,
+        });
+        const newQuestionSnapshot = await newQuestionRef.get();
+        return newQuestionSnapshot.data();
+      } catch (e) {
+        throw new ApolloError(e);
+      }
+    },
+    async addAnswer(root: null, args: any) {
+      try {
+        const { questionId, contents } = args;
+        const newAnswerRef = db.collection('answers').doc();
+        await newAnswerRef.set({
+          id: newAnswerRef.id,
+          contents,
+          questionId,
+        });
+        const newAnswerSnapshot = await newAnswerRef.get();
+        return newAnswerSnapshot.data();
+      } catch (e) {
+        throw new ApolloError(e);
+      }
+    }
+  },
+  Question: {
+    async answers(question: IQuestion) {
+      try {
+        const answerSnapshot = await db.collection('answers').where('questionId', '==', question.id).get();
+        return answerSnapshot.docs.map((snapshot) => {
+          return snapshot.data();
+        });
+      } catch (e) {
+        throw new ApolloError(e);
+      }
+    }
+  }
+}
+
+// graphql server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+server.applyMiddleware({ app });
+
+// server start!
+app.listen(7777, () => {
+  console.log('Server ready!');
+});
